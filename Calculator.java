2f5d49461e059520214735f9de02f17fd9d58b85
Yibing <zhang60638@gmail.com>
2015-11-09 18:17:45 -0500

    class exercise

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator{
+	public static double getSum(double [] num){
+	double	sum=0.0;
+	for(int i=0;i<num.length;i++){
+	sum+=num[i];
+	}
+	return sum;
+}
+	public static double getAverage(double [] num){
+	double	sum=0.0;
+	for(int i=0; i<num.length;i++){
+	sum+=num[i];
+	}
+	return(sum/num.length);
+}
+	public static double getProduct(double [] num){
+	double product=1;
+	for(int i=0;i<num.length;i++){
+	product*=num[i];
+	}
+	return product;
+}
+	public static double getFactorial(double number){
+	double factorial =1;
+	for(int i=0; i<number;i++){
+	factorial=number*i;
+	}
+	return factorial;
+
+}
+}
---
+++ CalculatorDriver.java
@@ -0,0 +1,18 @@
+public class CalculatorDriver{
+	public static void main(String [] args){
+	double [] num =new double[3];
+	num[0]=1.3;
+	num[1]=2.7;
+	num[2]=5.0;
+	System.out.println(Calculator.getAverage(num));
+	System.out.println(Calculator.getSum(num));
+	System.out.println(Calculator.getProduct(num));
+	System.out.println(Calculator.getFactorial(7));
+
+
+
+
+
+
+}
+}
